ec4957ce6392857d995ae6d19e336b8435600a90
PravinElangovan <115397005+PravinElangovan@users.noreply.github.com>
2023-04-16 14:25:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const student = {
+  name: "John Doe"
+};
+
+// Add a property to the Object prototype called getKeys()
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Call the getKeys() method on the student object
+console.log(student.getKeys());
